6f1bb7d3ea5b19d148231bcb5ec45bf656ba3dd4
Bob Spunt <bobspunt@gmail.com>
2015-07-06 15:19:10 -0700

    initial commit

---
+++ abc123/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN">
+<html>
+<head>
+<title>Online IAT</title>	
+<link type="text/css" href="core/css/overcast/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
+<style type="text/css"> @import "core/css/iat.css";</style>	
+<script type="text/javascript" src="core/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="core/js/jquery-ui-1.8.18.custom.min.js"></script>
+<script type="text/javascript" src="core/js/IAT.js"></script>
+<script type="text/javascript">
+	initialize();
+</script>
+</head>
+
+<body>
+    
+
+<div id="instructions">
+   Javascript must be enabled for the application to run. <br><br>
+ To check if your javascript is enabled, please click <a href="http://www.isjavascriptenabled.com/"target="_blank">here</a><br><br>
+ If your javascript is enabled, and you still see this error, then other browser add-ons or extensions might be preventing this program from running. In that case, this error message will go away by re-opening this page using a new private or incognito window. <br><br>
+ To do this, return to the survey, right click on the link in the survey and select<b> Open Link in Private(or Incognito) Window</b>
+ 
+</div>
+
+
+
+</body>
